6cd72a27ba8083cea007c4b2fac4ded7d909edfc
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-20 09:49:50 +0530

    [Puneeth] Modified Employee Attendence

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -10,7 +10,20 @@ namespace EmployeeWageCompute
     {
         private static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //UC1- EmployeeAttendence
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
