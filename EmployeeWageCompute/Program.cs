2da68eadc7af8feec5d184a49627fe19b6a11807
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-20 09:46:23 +0530

    [Puneeth]

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -1,8 +1,17 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompute
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Welcome to Employee Wage Computation");
-        Console.ReadLine();
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            Console.ReadLine();
+        }
     }
 }
