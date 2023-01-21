30e833150257cb326f26635ca35534364c2f3b3e
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-21 10:01:14 +0530

    20days of employee wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -12,33 +12,41 @@ namespace EmployeeWageCompute
         {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
+            const int IS_MAX_WORKING_DAY = 20;
             const int IS_EMP_RATE_PER_HR = 20;
-            int empHour = 0, empWage;
+            int empHour = 0, empWage=0, day, totalWage=0;
             Console.WriteLine("Welcome to Employee Wage Computation");
 
-            //UC4-Switch Case
-            Random random = new Random();
-            int empAttendence = random.Next(0, 3);
-            switch (empAttendence)
+            //UC4-Employee wage 20 days
+            
+            for (day = 1; day <= IS_MAX_WORKING_DAY; day++)
+
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is Present");
-                    empHour = 8;
-                    break;
+                Random random = new Random();
+                int empAttendence = random.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("\nEmployee is Present");
+                        empHour = 8;
+                        break;
 
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is Present for Half day");
-                    empHour = 4;
-                    break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("\nEmployee is Present for Half day");
+                        empHour = 4;
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    //empHour = 0;
-                    break;
+                    default:
+                        Console.WriteLine("\nEmployee is Absent");
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * IS_EMP_RATE_PER_HR;
+                //Console.WriteLine("Employee Wage: " + empWage);
+                Console.WriteLine("day{0} Employee Wage: {1}" ,day, empWage);
+                totalWage += empWage;
             }
-            empWage = empHour * IS_EMP_RATE_PER_HR;
-            //Console.WriteLine("Employee Wage: " + empWage);
-            Console.WriteLine("Employee Wage {0}", empWage);
+            Console.WriteLine("Total Employee wage for {0} days: {1}", (day - 1), totalWage);
             Console.ReadLine();
         }
     }
