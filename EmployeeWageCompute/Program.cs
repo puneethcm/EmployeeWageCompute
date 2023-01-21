baa5c50a9a77fd2bc119410026a83de70f3ec23a
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-21 09:03:21 +0530

    Calculating Part Time Wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -10,25 +10,31 @@ namespace EmployeeWageCompute
     {
         private static void Main(string[] args)
         {
-            const int FULL_TIME = 1;
-            const int EMP_RATE_PER_HR = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int IS_EMP_RATE_PER_HR = 20;
             int empHour = 0, empWage=0;
             Console.WriteLine("Welcome to Employee Wage Computation");
 
             //UC2-Daily wages
             Random random = new Random();
-            int empAttendence = random.Next(0, 2);
-            if (empAttendence == FULL_TIME)
+            int empAttendence = random.Next(0, 3);
+            if (empAttendence == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHour = 8;
+            }
+            else if(empAttendence== IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Present Half Day");
+                empHour = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
                 empHour = 0;
             }
-            empWage = empHour * EMP_RATE_PER_HR;
+            empWage = empHour * IS_EMP_RATE_PER_HR;
             //Console.WriteLine("Employee Wage: " + empWage);
             Console.WriteLine("Employee Wage {0}", empWage);
             Console.ReadLine();
